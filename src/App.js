54f6264d6b3bcf4aacee855095224473b9992c9f
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-13 14:26:49 +0700

    Refactor app design and implement counter functionality
    The CSS and structure of the App.js and App.css files have been refactored to change the overall design of the app. The changes now replace the old design with a new design that hosts a simple counter with increment, decrement, and reset buttons.
    
    Moreover, the functional component in App.js was changed to a class component to make use of the state and implement counter functionality. The counter increases, decreases, or resets based on user interaction with the buttons. This change was made to provide an interactive feature for the users to utilize.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    if (this.state.count < 10) {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }
+  }
+
+  decrementCount = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1
+      });
+    }
+  }
+
+  resetCount = () => {
+    this.setState({
+      count: 0
+    });
+  }
+
+  render() {
+    return (
+        <div className="container">
+          <button onClick={this.decrementCount} className="button-decrement">-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.incrementCount} className="button-increment">+</button>
+          <button onClick={this.resetCount}>Reset</button>
+        </div>
+    );
+  }
 }
 
 export default App;
